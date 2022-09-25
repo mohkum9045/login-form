f981cb7f16b3f8cf3f1cafe6002fd8ead61b9ed8
Mohit <mohkum9045@gmail.com>
2022-09-25 10:21:19 +0530

    Login form created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [alldata, setAllData] = useState([]);
+
+  const submitform = (event) => {
+    event.preventDefault();
+    const data = { email: email, password: password };
+    setAllData([...alldata, data]);
+    console.log("{ email }, { password }");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form action="" onSubmit={submitform}>
+        <label htmlFor="email">Email</label>
+        <input
+          name="email"
+          id="email"
+          autoComplete="off"
+          value={email}
+          placeholder="Enter your Email"
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}></input>
+
+        <label htmlFor="password">Password</label>
+        <input
+          name="password"
+          id="password"
+          autoComplete="off"
+          value={password}
+          placeholder="Enter the Password"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+          type="password"></input>
+        <button>Submit</button>
+      </form>
+
+      <div>
+        {alldata.map((val) => {
+          return (
+            <div>
+              <p>{val.email}</p>
+              <p>{val.password}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
